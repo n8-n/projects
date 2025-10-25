3a6cde11c1c3acaa9efc6d9fa8337b9e08fbc7ed
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2025-10-25 16:34:05 +0100

    add supercollider and scheme files

---
+++ scheme/sicp-ch3-1.rkt
@@ -0,0 +1,310 @@
+#lang sicp
+
+
+;; 3.3.2 Queues
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called on empty queue" queue)
+      (car (front-ptr queue))))
+
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+
+;; Exercise 3.21
+(define (print-queue queue)
+  (front-ptr queue))
+
+
+
+;; Exercise 3.22
+
+(define (make-queue2)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+
+    (define (empty?)
+      (null? front-ptr))
+
+    (define (add-to-queue x)
+      (let ((new-pair (cons x '())))
+        (cond ((empty?)
+               (set! front-ptr new-pair)
+               (set! rear-ptr new-pair)
+               front-ptr)
+              (else
+               (set-cdr! rear-ptr new-pair)
+               (set! rear-ptr new-pair)
+               front-ptr))))
+    (define (delete-from-queue)
+      (cond ((empty?)
+             (error "Cannot delete from empty queue!"))
+            (else
+             (set! front-ptr (cdr front-ptr))
+             front-ptr)))
+    (define (print-queue) front-ptr)
+    (define (front-queue) (car front-ptr))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'add) add-to-queue)
+            ((eq? m 'delete) delete-from-queue)
+            ((eq? m 'print) print-queue)
+            ((eq? m 'front) front-queue)
+            (else
+             (error "Unknown option --" m))))
+    dispatch))
+
+
+;; Exercise 3.23
+(define (make-deque)
+  (let ((front-ptr '())
+        (rear-ptr '()))
+
+    (define (make-node value prev next)
+      (list value prev next))
+
+    (define (get-value node) (car node))
+    (define (get-prev node) (cadr node))
+    (define (set-prev! node x) (set-car! (cdr node) x))
+    (define (get-next node) (caddr node))
+    (define (set-next! node x) (set-car! (cddr node) x))
+
+    (define (empty?)
+      (null? front-ptr))
+
+    (define (add-to-front x)
+      (cond ((empty?)
+             (let ((new-node (make-node x '() '())))
+               (set! front-ptr new-node)
+               (set! rear-ptr new-node)
+               (print-queue)))
+              (else
+               (let ((front-node (make-node x '() front-ptr)))
+                 (set-prev! front-ptr front-node)
+                 (set! front-ptr front-node)
+                 (print-queue)))))
+    (define (add-to-rear x)
+      (cond ((empty?)
+             (let ((new-node (make-node x '() '())))
+               (set! front-ptr new-node)
+               (set! rear-ptr new-node)
+               (print-queue)))
+            (else
+             (let ((rear-node (make-node x rear-ptr '())))
+               (set-next! rear-ptr rear-node)
+               (set! rear-ptr rear-node)
+               (print-queue)))))
+    
+    (define (delete-from-front)
+      (cond ((empty?)
+             (error "Cannot delete from empty queue!"))
+            (else
+             (set! front-ptr (get-next front-ptr))
+             (set-prev! front-ptr '())
+             (print-queue))))
+    (define (delete-from-rear)
+      (cond ((empty?)
+             (error "Cannot delete from empty queue!"))
+            (else
+             (set! rear-ptr (get-prev rear-ptr))
+             (set-next! rear-ptr '())
+             (print-queue))))
+    
+    (define (print-queue)
+      (define (loop l curr)
+        (if (null? curr)
+            l
+            (loop (append l (list (get-value curr)))
+                  (get-next curr))))
+      (loop '() front-ptr))
+      
+    (define (front-queue) (car front-ptr))
+
+    (define (raw-print) front-ptr)
+    
+    (define (dispatch m)
+      (cond ((eq? m 'add-front) add-to-front)
+            ((eq? m 'add-rear) add-to-rear)
+            ((eq? m 'delete-front) delete-from-front)
+            ((eq? m 'delete-rear) delete-from-rear)
+            ((eq? m 'print) print-queue)
+            ((eq? m 'front) front-queue)
+            ((eq? m 'raw-print) raw-print)
+            (else
+             (error "Unknown option --" m))))
+    dispatch))
+
+
+(define (init-deque . args)
+  (let ((deque (make-deque)))
+    (define (loop l)
+      (if (null? l)
+          deque
+          (begin ((deque 'add-rear) (car l))
+                 (loop (cdr l)))))
+    (loop args)))
+
+;; (define q2 (make-deque))
+;; ((q2 'add-front) 'a)
+;; ((q2 'add-front) 'b)
+;; ((q2 'add-rear) 'd)
+;; ((q2 'add-rear) 'c)
+
+
+
+;; 3.3.3 Tables
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table)))))
+  'ok)
+
+(define (make-table1)
+  (list '*table*))
+
+
+(define (lookup2 key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (cdr record)
+              false))
+        false)))
+
+(define (insert2! key-1 key-2 value table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr table))))
+          (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key-1
+                              (cons key-2 value))
+                        (cdr table)))))
+  'ok)
+
+
+;; exercise 3.24 & 3.25
+(define (make-table equality)
+  (let ((local-table (list '*table*)))
+    (define (assoc key records)
+      (cond ((null? records) false)
+            ((equality key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+
+    (define (lookup-general key table)
+      (let ((record (assoc key (cdr table))))
+        (if record
+            (cdr record)
+            false)))
+    
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            ((eq? m 'print) local-table)
+            (else (error "Unknown operation -- TABLE" m))))
+
+    dispatch))
+
+
+
+        ;; exercise 3.26
+(define (make-table-tree)
+  (list '*table-tree*))
+
+(define (tree-node key value)
+  (list key value '() '()))
+
+
+
+;; Exercise 3.27
+(define (memoize f)
+  (let ((table (make-table1)))
+    (lambda (x)
+      (let ((previous-result (lookup x table)))
+        (or previous-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
+
+(define memo-fib
+  (memoize (lambda (n)
+             (cond ((= n 0) 0)
+                   ((= n 1) 1)
+                   (else (+ (memo-fib (- n 1))
+                            (memo-fib (- n 2))))))))
+
+
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+
---
+++ scheme/sicp-ch3-circuits.rkt
@@ -0,0 +1,394 @@
+#lang sicp
+
+;; Queues
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called on empty queue" queue)
+      (car (front-ptr queue))))
+
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+
+
+
+
+
+(define inverter-delay 2)
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define and-gate-delay 3)
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1)
+              (= s2 1)) 1)
+        ((or (= s1 0)
+             (= s2 0)) 0)
+        (else (error "Invalid signal" (s1 s2)))))
+
+(define (create-2-input-gate logic gate-delay)
+  (lambda (a1 a2 output)
+    (let ((new-value
+           (logic (get-signal a1) (get-signal a2))))
+      (after-delay gate-delay
+                   (lambda ()
+                     (set-signal! output new-value))))))
+
+
+;; exercise 3.28
+(define or-gate-delay 5)
+
+;; (define (or-gate a1 a2 output)
+;;   (let ((or-action-procedure
+;;          ((create-2-input-gate logical-or or-gate-delay)
+;;           a1
+;;           a2
+;;           output)))
+;;     (add-action! a1 or-action-procedure)
+;;     (add-action! a2 or-action-procedure)
+;;     'ok))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+
+(define (logical-or s1 s2)
+  (cond ((and (= s1 0)
+              (= s2 0)) 0)
+        ((or (= s1 1)
+             (= s2 1)) 1)
+        (else (error "Invalid signal" (s1 s2)))))
+
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+
+;; exercise 3.29
+(define (nand a b z)
+  (let ((x (make-wire)))
+    (and-gate a b x)
+    (inverter x z)
+    'ok))
+
+;; delay = and-gate-delay + (inverter-delay * 3)
+(define (or2 a b z)
+  (let ((x (make-wire)) (y (make-wire)))
+    (inverter a x)
+    (inverter b y)
+    (nand x y z)))
+
+
+;; Exercise 3.30
+(define (make-wire-list n)
+  (let ((wire (make-wire)))
+    (if (= n 0)
+        '()
+        (cons wire (make-wire-list (- n 1))))))
+
+
+(define (map f ls)
+  (if (null? ls)
+      '()
+      (cons (f (car ls))
+            (map f (cdr ls)))))
+
+(define (equal-lengths . ls)
+  (apply = (map length ls)))
+
+;; doesn't work
+(define (ripple-carry-adder an bn sn c-in-init c-out-final)
+  (define (adder-loop a b s c)
+    (let ((new-carry (make-wire)))
+      (if (= (length s) 1)
+          (full-adder (car a) (car b) c (car s) c-out-final)
+          (begin
+            (adder-loop (cdr a) (cdr b) (cdr s) new-carry)
+            (full-adder (car a) (car b) c (car s) new-carry)))))
+  
+  (if (not (equal-lengths an bn sn))
+      (error "a b and s are not equal lengths!")
+      (adder-loop an bn sn c-in-init))
+  'ok)
+
+	
+
+(define (ripple-carry-adder2 As Bs Ss C)
+  (let ((carry (make-wire)))
+    (if (null? As)
+        (set-signal! C 0)
+        (begin
+          (ripple-carry-adder2 (cdr As) (cdr Bs) (cdr Ss) carry)
+          (full-adder (car As) (car Bs) carry (car Ss) C)))))
+
+
+
+(define (make-wire)
+  (let ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+
+(define (get-wire-list-signals wires)
+  (if (null? wires)
+      '()
+      (cons (get-signal (car wires))
+            (get-wire-list-signals (cdr wires)))))
+
+(define (set-wire-list-signals! wires vals)
+  (define (loop w v)
+    (if (null? w)
+        'done
+        (begin
+          (set-signal! (car w) (car v))
+          (loop (cdr w) (cdr v)))))
+  (if (equal-lengths wires vals)
+      (loop wires vals)
+      (error "Mismatched list lengths!")))
+
+
+;; AGENDA -----------
+(define (make-agenda) (list 0))
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+
+(define the-agenda (make-agenda))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr! segments (cons
+                                  (make-new-time-segment time action)
+                                  (cdr segments)))
+              (add-to-segments! rest)))))
+  
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (begin
+      (delete-queue! q)
+      (if (empty-queue? q)
+          (set-segments! agenda (rest-segments agenda))))))
+    
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty!")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+
+;; ---------------------------
+;; Decimal and binary conversions
+;; ---------------------------
+(define (decimal-to-binary number bits)
+  (define (loop result dividend)
+    (let ((quo (quotient dividend 2))
+          (rem (remainder dividend 2)))
+      (if (= (length result) bits)
+          result
+          (loop (cons rem result) quo))))
+    (loop '() number))
+
+
+;; Convert decimal number to list of 8 bits
+(define (to-8-bit-binary n)
+  (decimal-to-binary n 8))
+
+(define (binary-to-decimal n)
+  (define (loop result position numbers)
+    (if (null? numbers)
+        result
+        (let ((position-mult (* (car numbers)
+                                (expt 2 position))))
+          (loop (+ result position-mult)
+                (inc position)
+                (cdr numbers)))))
+  (loop 0 0 (reverse n)))
+
+
+
+(define i1 (make-wire))
+(define i2 (make-wire))
+(define sum (make-wire))
+(define carry (make-wire))
+
+
+(define a1 (make-wire-list 8))
+(define a2 (make-wire-list 8))
+
+(define s1 (make-wire-list 8))
+(define c1 (make-wire))
+(define c2 (make-wire))
+
+(ripple-carry-adder2 a1 a2 s1 c1)
+
+(set-wire-list-signals! a1 (to-8-bit-binary 17))
+(set-wire-list-signals! a2 (to-8-bit-binary 31))
