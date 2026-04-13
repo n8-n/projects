579ea1f3cec4f5642092628b6566c723d1576a0a
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2026-04-13 13:17:58 +0100

    sicp ch4 exercises

--- scheme/sicp-ch4-evaluator_lazy.rkt
+++ scheme/sicp-ch4-evaluator_lazy.rkt
@@ -13,25 +13,64 @@
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (my-eval (cond->if exp) env))
         ((application? exp)
-         (my-apply (my-eval (operator exp) env)
-                   (list-of-values (operands exp) env)))
+         (my-apply (actual-value (operator exp) env)
+                   (operands exp)
+                   env))
         (else
          (error "Unknown expression type: EVAL" exp))))
 
+(define (actual-value exp env)
+  (force-it (my-eval exp env)))
 
-(define (my-apply procedure arguments)
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result (actual-value (thunk-exp obj) (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           (set-car! (cdr obj) result)    ; replace exp with its value 
+           (set-cdr! (cdr obj) '())       ; forget unneeded env
+           result))
+        ((evaluated-thunk? obj) (thunk-value obj))
+        (else obj)))
+
+(define (delay-it exp env)
+  (list 'thunk exp env))
+
+(define (thunk? obj)
+  (tagged-list? obj 'thunk))
+(define (thunk-exp thunk) (cadr thunk))
+(define (thunk-env thunk) (caddr thunk))
+
+(define (evaluated-thunk? obj)
+  (tagged-list? obj 'evaluated-thunk))
+
+(define (thunk-value evaluated-thunk) (cadr evaluated-thunk))
+
+(define (my-apply procedure arguments env)
   (cond ((primitive-procedure? procedure)
-         (apply-primitive-procedure procedure arguments))
+         (apply-primitive-procedure
+           procedure
+           (list-of-arg-values arguments env)))
         ((compound-procedure? procedure)
          (eval-sequence
           (procedure-body procedure)
           (extend-environment
            (procedure-parameters procedure)
-           arguments
-           (procedure-environment procedure))))
+             (list-of-delayed-args arguments env)
+             (procedure-environment procedure))))
         (else
          (error "Unknown procedure type: APPLY" procedure))))
 
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (actual-value (first-operand exps) env)
+            (list-of-arg-values (rest-operands exps) env))))
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (delay-it (first-operand exps) env)
+            (list-of-delayed-args (rest-operands exps) env))))
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
@@ -41,7 +80,7 @@
 
 
 (define (eval-if exp env)
-  (if (true? (my-eval (if-predicate exp) env))
+  (if (true? (actual-value (if-predicate exp) env))
       (my-eval (if-consequent exp) env)
       (my-eval (if-alternative exp) env)))
 
@@ -305,7 +344,7 @@
   (prompt-for-input input-prompt)
   (let ((input (read))
         (starttime (runtime)))
-    (let ((output (my-eval input the-global-environment)))
+    (let ((output (actual-value input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)
       (if logging
