025ccf37aaf77dc067c79021f1d66156a2a5f23d
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2026-06-06 18:57:39 +0100

    Add machine code for sicp ch5

--- sicp/ch5/01-registers.rkt
+++ sicp/ch5/01-registers.rkt
@@ -185,3 +185,102 @@
 
 (define (get-register machine reg-name)
   ((machine 'get-register) reg-name))
+
+
+;; Assembler
+(define (assemble controller-text machine)
+  (extract-labels controller-text
+                  (lambda (insts labels)
+                    (update-insts! insts labels machine)
+                    insts)))
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels (cdr text)
+                      (lambda (insts labels)
+                        (let ((next-inst (car text)))
+                          (if (symbol? next-inst)
+                              (receive insts (cons (make-label-entry next-inst insts)
+                                                   labels))
+                              (receive (cons (make-instruction next-inst) insts)
+                                  labels)))))))
+
+(define (update-insts! insts labels machine)
+  (let ((px (get-register machine 'pc))
+        (flag (get-register machine 'flag))
+        (stack (machine 'stack))
+        (ops (machine 'operations)))
+    (for-each
+     (lambda (inst)
+       (set-instruction-execution-proc!
+        inst
+        (make-execution-procedure (instruction-text inst) labels machine
+                                  pc flag stack ops)))
+     insts)))
+
+(define (make-instuction text) (cons text '()))
+(define (instruction-text inst) (car insts))
+(define (instruction-execution-proc inst) (cdr inst))
+(define (set-instruction-execution-proc! inst proc) (set-cdr! inst proc))
+
+(define (make-label-entry label-name insts)
+  (cond label-name insts))
+
+(define (lookup-label labels label-name)
+  (let ((val (assoc label-name labels)))
+    (if val
+        (cdr val)
+        (error "Undefined label -- ASSEMBLE" label-name))))
+
+
+(define (make-execution-procedure inst labels machine
+                                  pc flag stack ops)
+  (cond ((eq? (car inst) 'assign)
+         (make-assign inst machine labels ops pc))
+        ((eq? (car inst) 'test)
+         (make-test inst machine labels ops flag pc))
+        ((eq? (car inst) 'branch)
+         (make-branch inst machine labels flag pc))
+        ((eq? (car inst) 'goto)
+         (make-goto inst machine labels pc))
+        ((eq? (car inst) 'save)
+         (make-save inst machine stack pc))
+        ((eq? (car inst) 'restore)
+         (make-restore inst machine stack pc))
+        ((eq? (car inst) 'perform)
+         (make-perform inst machine labels ops pc))
+        (else (error "Unknown instruction type -- ASSEMBLE" inst))))
+
+(define (make-assign inst machin labels operations pc)
+  (let ((target (get-register machine (assign-reg-name inst)))
+        (value-exp (assign-value-exp inst)))
+    (let ((value-proc
+           (if (operation-exp? value-exp)
+               (make-operation-exp
+                value-exp machine labels operations)
+               (make-primitive-exp
+                (car value-exp) machine labels))))
+      (lambda () ; execution procedure for assign
+        (set-contents! target (value-proc))
+        (advance-pc pc)))))
+
+(define (assign-reg-name assign-instructions) (cadr assign-instructions))
+(define (assign-value-exp assign-instructions) (cddr assign-instructions))
+
+(define (advance-pc pc)
+  (set-contents! pc (cdr (get-contents pc))))
+
+(define (make-test inst machine labels operations flag pc)
+  (let ((condition (test-condition inst)))
+    (if (operation-exp? condition)
+        (let ((condition-proc
+               (make-operation-exp condition machine labels operations)))
+          (lambda ()
+            (set-contents! flag (condition-proc))
+            (advance-pc pc)))
+        (error "Bad TEST instruction -- ASSEMBLE" inst))))
+
+(define (test-condition test-instruction) (cdr test-instruction))
+
+;; Continue at make-branch
