3354505a86564a4d7009bdf370a3464cbf37d6ef
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2026-06-07 18:33:16 +0100

    finish machine code for ch5 sicp

--- sicp/ch5/01-registers.rkt
+++ sicp/ch5/01-registers.rkt
@@ -1,80 +1,4 @@
-;; Exercise 5.2
-(define fac-iter-machine1
-  '(controller
-    factorial
-      (assign product 1)
-      (assign count 1)
-    iter
-      (test (op >) (reg count) (reg n))
-      (branch (label fac-done))
-      (assign product (op *) (reg product) (reg count))
-      (assign count (op +1) (reg count))
-      (goto (label iter))
-    fac-done))
-    
-
-;; Exercise 5.3
-(define sqrt-1
-  '(controller
-    sqrt
-      (assign guess (const 1.0))
-    iter
-      (test (op good-enough?) (reg guess))
-      (branch (label sqrt-done))
-      (assign guess (op improve) (reg guess))
-    sqrt-done))
-
-(define sqrt-2
-  '(controller
-    sqrt
-      (assign guess (const 1.0))
-    iter
-    good-enough?
-      (assign t (op square) (reg guess))
-      (assign t (op -) (reg t) (reg x))
-      (assign t (op abs) (reg t))
-      (test (op <) (reg t) (const 0.001))
-      (branch (label sqrt-done))
-    improve
-      (assign t (op /) (reg x) (reg guess))
-      (assign guess (op avg) (reg t) (reg guess))
-      (goto (label iter))
-    sqrt-done))
-      
-;; Exercise 5.4
-
-(define expt-1
-  '(controller
-      (assign continue (label expt-done))
-    expt-loop
-      (test (op =) (reg n) (const 0))
-      (branch (label base-case))
-      ;; setup recursive call
-      (save continue)
-      (assign n (op -) (reg n) (const 1))
-      (assign continue (label after-expt))
-      (goto (label expt-loop))
-    after-expt
-      (restore continue)
-      (assign val (op *) (reg b) (reg val))
-      (goto (reg continue))
-    base-case
-      (assign val (const 1))
-      (goto (reg continue))
-    expt-done))
-
-(define expt-2
-  '(controller
-     (assign (reg count) (reg n))
-     (assign (reg n) (const 1))
-   expt-iter
-     (test (op =) (reg count) (const 0))
-     (branch (label expt-done))
-     (assign count (op -) (reg count) (const 1))
-     (assign n (op *) (reg b) (reg n))
-     (goto (label expt-iter))
-   expt-done))
-     
+#lang sicp
 
 ;; Machine model
 (define (make-machine register-names ops controller-text)
@@ -82,7 +6,7 @@
     (for-each (lambda (register-name)
                 ((machine 'allocate-register) register-name))
               register-names)
-    ((machine 'install-operations) ops)
+    ((machine 'install-operation) ops)
     ((machine 'install-instruction-sequence)
      (assemble controller-text machine))
     machine))
@@ -99,7 +23,7 @@
 (define (get-contents register)
   (register 'get))
 
-(define (set-contents register value)
+(define (set-contents! register value)
   ((register 'set) value))
 
 (define (make-stack)
@@ -136,9 +60,9 @@
         (the-instruction-sequence '()))
     (let ((the-ops
            (list (list 'initialise-stack
-                       (lambda () (stack 'initialise))))
-           (register-table
-            (list (list 'pc pc) (list 'flag flag)))))
+                       (lambda () (stack 'initialise)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
       (define (allocate-register name)
         (if (assoc name register-table)
             (error "Multiply defined register: " name)
@@ -164,8 +88,8 @@
                (execute))
               ((eq? message 'install-instruction-sequence)
                (lambda (seq) (set! the-instruction-sequence seq)))
-              ((eq? message 'allocate-registers) allocate-registers)
-              ((eq? message 'get-registers) lookup-register)
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
               ((eq? message 'install-operation)
                (lambda (ops) (set! the-ops (append the-ops ops))))
               ((eq? message 'stack) stack)
@@ -175,6 +99,20 @@
 
 (define (start machine)
   (machine 'start))
+
+;; start machine with initialised registers, and print final return
+(define (run machine reg-bindings return)
+  (let ((reg-setup (lambda (reg-binding)
+                     (set-register-contents! machine
+                                             (car reg-binding)
+                                             (cadr reg-binding)))))
+    (for-each reg-setup reg-bindings)
+    (machine 'start)
+    (get-register-contents machine return)))
+  
+(define (start-print machine register)
+  (machine 'start)
+  (get-register-contents machine register))
 
 (define (get-register-contents machine register-name)
   (get-contents (get-register machine register-name)))
@@ -207,7 +145,7 @@
                                   labels)))))))
 
 (define (update-insts! insts labels machine)
-  (let ((px (get-register machine 'pc))
+  (let ((pc (get-register machine 'pc))
         (flag (get-register machine 'flag))
         (stack (machine 'stack))
         (ops (machine 'operations)))
@@ -219,13 +157,13 @@
                                   pc flag stack ops)))
      insts)))
 
-(define (make-instuction text) (cons text '()))
-(define (instruction-text inst) (car insts))
+(define (make-instruction text) (cons text '()))
+(define (instruction-text inst) (car inst))
 (define (instruction-execution-proc inst) (cdr inst))
 (define (set-instruction-execution-proc! inst proc) (set-cdr! inst proc))
 
 (define (make-label-entry label-name insts)
-  (cond label-name insts))
+  (cons label-name insts))
 
 (define (lookup-label labels label-name)
   (let ((val (assoc label-name labels)))
@@ -252,7 +190,7 @@
          (make-perform inst machine labels ops pc))
         (else (error "Unknown instruction type -- ASSEMBLE" inst))))
 
-(define (make-assign inst machin labels operations pc)
+(define (make-assign inst machine labels operations pc)
   (let ((target (get-register machine (assign-reg-name inst)))
         (value-exp (assign-value-exp inst)))
     (let ((value-proc
@@ -283,4 +221,211 @@
 
 (define (test-condition test-instruction) (cdr test-instruction))
 
-;; Continue at make-branch
+(define (make-branch inst machine labels flag pc)
+  (let ((dest (branch-dest inst)))
+    (if (label-exp? dest)
+        (let ((insts (lookup-label labels (label-exp-label dest))))
+          (lambda ()
+            (if (get-contents flag)
+                (set-contents! pc insts)
+                (advance-pc pc))))
+        (error "Bad BRANCH instruction -- ASSEMBLE" inst))))
+
+(define (branch-dest branch-instruction)
+  (cadr branch-instruction))
+
+(define (make-goto inst machine labels pc)
+  (let ((dest (goto-dest inst)))
+    (cond ((label-exp? dest)
+           (let ((insts (lookup-label labels (label-exp-label dest))))
+             (lambda () (set-contents! pc insts))))
+          ((register-exp? dest)
+           (let ((reg (get-register machine (register-exp-reg dest))))
+             (lambda () (set-contents! pc (get-contents reg)))))
+          (else (error "Bad GOTO instruction -- ASSEMBLE" inst)))))
+
+(define (goto-dest goto-instruction)
+  (cadr goto-instruction))
+
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine (stack-inst-reg-name inst))))
+    (lambda ()
+      (push stack (get-contents reg))
+      (advance-pc pc))))
+
+(define (make-restore inst machine stack pc)
+  (let ((reg (get-register machine (stack-inst-reg-name inst))))
+    (lambda ()
+      (set-contents! reg (pop stack))
+      (advance-pc pc))))
+
+(define (stack-inst-reg-name stack-instruction)
+  (cadr stack-instruction))
+
+(define (make-perform inst machine labels operations pc)
+  (let ((action (perform-action inst)))
+    (if (operation-exp? action)
+        (let ((action-proc
+               (make-operation-exp action machine labels operations)))
+          (lambda ()
+            (action-proc)
+            (advance-pc pc)))
+        (error "Bad PERFORM instruction -- ASSEMBLE" inst))))
+
+(define (perform-action inst) (cdr inst))
+
+(define (make-primitive-exp exp machine labels)
+  (cond ((constant-exp? exp)
+         (let ((c (constant-exp-value exp)))
+           (lambda () c)))
+        ((label-exp? exp)
+         (let ((insts (lookup-label labels (label-exp-label exp))))
+           (lambda () insts)))
+        ((register-exp? exp)
+         (let ((r (get-register machine (register-exp-reg exp))))
+           (lambda () (get-contents r))))
+        (else (error "Unknown expression type -- ASSEMBLE" exp))))
+
+(define (register-exp? exp) (tagged-list? exp 'reg))
+(define (register-exp-reg exp) (cadr exp))
+(define (constant-exp? exp) (tagged-list? exp 'const))
+(define (constant-exp-value exp) (cadr exp))
+(define (label-exp? exp) (tagged-list? exp 'label))
+(define (label-exp-label exp) (cadr exp))
+
+(define (tagged-list? exp tag)
+  (if (pair? exp)
+      (eq? (car exp) tag)
+      false))
+
+(define (make-operation-exp exp machine labels operations)
+  (let ((op (lookup-prim (operation-exp-op exp) operations))
+        (aprocs
+         (map (lambda (e) (make-primitive-exp e machine labels))
+              (operation-exp-operands exp))))
+    (lambda ()
+      (apply op (map (lambda (p) (p)) aprocs)))))
+
+(define (operation-exp? exp)
+  (and (pair? exp) (tagged-list? (car exp) 'op)))
+(define (operation-exp-op operation-exp)
+  (cadr (car operation-exp)))
+(define (operation-exp-operands operation-exp)
+  (cdr operation-exp))
+
+(define (lookup-prim symbol operations)
+  (let ((val (assoc symbol operations)))
+    (if val
+        (cadr val)
+        (error "Unknown operation -- ASSEMBLE" symbol))))
+
+
+;; shortcuts
+(define src! set-register-contents!)
+(define grc get-register-contents)
+
+
+;; Exercise 5.2
+(define fac-iter-machine1
+  '(controller
+    factorial
+      (assign product 1)
+      (assign count 1)
+    iter
+      (test (op >) (reg count) (reg n))
+      (branch (label fac-done))
+      (assign product (op *) (reg product) (reg count))
+      (assign count (op +1) (reg count))
+      (goto (label iter))
+    fac-done))
+    
+
+;; Exercise 5.3
+(define sqrt-1
+  '(controller
+    sqrt
+      (assign guess (const 1.0))
+    iter
+      (test (op good-enough?) (reg guess))
+      (branch (label sqrt-done))
+      (assign guess (op improve) (reg guess))
+    sqrt-done))
+
+(define (square x) (* x x))
+
+(define (avg a b)
+  (/ (+ a b) 2))
+
+(define sqrt-2
+  (make-machine
+   '(guess t x)
+   (list (list 'square square) (list '/ /) (list 'avg avg)
+          (list 'abs abs) (list '- -) (list '< <))
+  '(sqrt
+      (assign guess (const 1.0))
+    iter
+    ;;good-enough?
+      (assign t (op square) (reg guess))
+      (assign t (op -) (reg t) (reg x))
+      (assign t (op abs) (reg t))
+      (test (op <) (reg t) (const 0.001))
+      (branch (label sqrt-done))
+    improve
+      (assign t (op /) (reg x) (reg guess))
+      (assign guess (op avg) (reg t) (reg guess))
+      (goto (label iter))
+    sqrt-done)))
+
+
+;; Exercise 5.4
+(define expt-1
+  (make-machine
+   '(continue n b val)
+   (list (list '= =) (list '- -) (list '* *))
+   '(controller
+      (assign continue (label expt-done))
+    expt-loop
+      (test (op =) (reg n) (const 0))
+      (branch (label base-case))
+      ;; setup recursive call
+      (save continue)
+      (assign n (op -) (reg n) (const 1))
+      (assign continue (label after-expt))
+      (goto (label expt-loop))
+    after-expt
+      (restore continue)
+      (assign val (op *) (reg b) (reg val))
+      (goto (reg continue))
+    base-case
+      (assign val (const 1))
+      (goto (reg continue))
+    expt-done)))
+
+(define expt-2
+  '(controller
+     (assign (reg count) (reg n))
+     (assign (reg n) (const 1))
+   expt-iter
+     (test (op =) (reg count) (const 0))
+     (branch (label expt-done))
+     (assign count (op -) (reg count) (const 1))
+     (assign n (op *) (reg b) (reg n))
+     (goto (label expt-iter))
+   expt-done))
+     
+
+(define gcd-machine
+  (make-machine
+   '(a b t)
+   (list (list 'rem remainder) (list '= =))
+   '(test-b
+       (test (op =) (reg b) (const 0))
+       (branch (label gcd-done))
+       (assign t (op rem) (reg a) (reg b))
+       (assign a (reg b))
+       (assign b (reg t))
+       (goto (label test-b))
+       gcd-done)))
+
+(define d (make-new-machine))
+((d 'allocate-register) 't)
