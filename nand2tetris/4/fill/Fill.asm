ff816644848a3b25b36a660fbb6ee552a870ccde
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2026-09-12 19:34:15 +0100

    add partial solutions for module4

--- nand2tetris/4/fill/Fill.asm
+++ nand2tetris/4/fill/Fill.asm
@@ -7,4 +7,46 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+// get screen address and store in R0, 
+// which will be used as an address counter
+@SCREEN
+D=A
+@screenAddress
+M=D
+
+// final screen address constant
+@8192
+D=A
+@finalAddress
+M=D // init finalAddress to 8192
+
+@screenAddress
+D=M
+@finalAddress
+M=D+M   // set finalAddress to end of screenMemory (16384 + 8192)
+
+(LOOP)
+    // End if we've reached final address
+    @screenAddress
+    D=M
+    @finalAddress
+    D=D-M
+    @END
+    D;JEQ
+
+    // paint it black
+    @screenAddress
+    A=M
+    M=1 
+    M=-M    // -1 = 16 bit number with all 1s
+
+    @screenAddress
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
--- nand2tetris/4/mult/Mult.asm
+++ nand2tetris/4/mult/Mult.asm
@@ -6,4 +6,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+(INIT)
+    @R2
+    M=0
+
+(LOOP)
+    // check if R1 is 0
+    @R1
+    D=M
+    @END
+    D;JEQ
+    // decrement R1
+    @R1
+    M=M-1
+    // get R0
+    @R0
+    D=M
+    // get R3 and add
+    @R2
+    M=D+M
+    // Loop
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
